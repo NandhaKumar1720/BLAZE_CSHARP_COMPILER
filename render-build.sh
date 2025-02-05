d207a33137a7e720b210549fb926e2546cd7086b
NandhaKumar1720 <nandhakishorebhaskar1720@gmail.com>
2025-02-05 20:27:56 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Install dependencies for .NET and Node.js
+# Install dependencies for Mono and Node.js
 echo "Installing dependencies..."
 
-# Install .NET SDK
+# Install Mono (mcs compiler)
 apt-get update
-apt-get install -y dotnet-sdk-6.0 build-essential
+apt-get install -y mono-mcs build-essential
 
 # Install Node.js dependencies
 npm install
